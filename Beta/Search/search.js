b585895c1ab629edf5597c04f21797ebf87de011
Junjyoti Changmai <junjyotichangmai12@gmail.com>
2024-11-08 10:43:21 +0530

    updating

--- Beta/Search/search.js
+++ Beta/Search/search.js
@@ -1,4 +1,159 @@
-import { books } from "../books.js";
+const books = [
+    {
+        "title": "Computer Networks",
+        "author": "Andrew S. Tanenbaum",
+        "stock": 6,
+        "category": "Computer Science",
+        "bookid": "book100001"
+    },
+    {
+        "title": "Operating System Concepts",
+        "author": "Abraham Silberschatz",
+        "stock": 3,
+        "category": "Computer Science",
+        "bookid": "book100002"
+    },
+    {
+        "title": "Structural Analysis",
+        "author": "Russell C. Hibbeler",
+        "stock": 0,
+        "category": "Civil Engineering",
+        "bookid": "book100003"
+    },
+    {
+        "title": "Mechanics of Materials",
+        "author": "Ferdinand P. Beer",
+        "stock": 6,
+        "category": "Civil Engineering",
+        "bookid": "book100004"
+    },
+    {
+        "title": "Engineering Mechanics: Statics",
+        "author": "J.L. Meriam",
+        "stock": 5,
+        "category": "Civil Engineering",
+        "bookid": "book100005"
+    },
+    {
+        "title": "Fluid Mechanics",
+        "author": "Frank M. White",
+        "stock": 4,
+        "category": "Mechanical Engineering",
+        "bookid": "book100006"
+    },
+    {
+        "title": "Engineering Mechanics: Dynamics",
+        "author": "J.L. Meriam",
+        "stock": 0,
+        "category": "Mechanical Engineering",
+        "bookid": "book100007"
+    },
+    {
+        "title": "Thermodynamics: An Engineering Approach",
+        "author": "Yunus A. Çengel",
+        "stock": 8,
+        "category": "Mechanical Engineering",
+        "bookid": "book100008"
+    },
+    {
+        "title": "Introduction to Algorithms",
+        "author": "Thomas H. Cormen",
+        "stock": 5,
+        "category": "Computer Science",
+        "bookid": "book100009"
+    },
+    {
+        "title": "Artificial Intelligence: A Modern Approach",
+        "author": "Stuart Russell",
+        "stock": 0,
+        "category": "Computer Science",
+        "bookid": "book100010"
+    },
+    {
+        "title": "Computer Networks",
+        "author": "Andrew S. Tanenbaum",
+        "stock": 6,
+        "category": "Computer Science",
+        "bookid": "book100011"
+    },
+    {
+        "title": "Operating System Concepts",
+        "author": "Abraham Silberschatz",
+        "stock": 3,
+        "category": "Computer Science",
+        "bookid": "book100012"
+    },
+    {
+        "title": "Structural Analysis",
+        "author": "Russell C. Hibbeler",
+        "stock": 3,
+        "category": "Civil Engineering",
+        "bookid": "book100013"
+    },
+    {
+        "title": "Mechanics of Materials",
+        "author": "Ferdinand P. Beer",
+        "stock": 6,
+        "category": "Civil Engineering",
+        "bookid": "book100014"
+    },
+    {
+        "title": "Engineering Mechanics: Statics",
+        "author": "J.L. Meriam",
+        "stock": 5,
+        "category": "Civil Engineering",
+        "bookid": "book100015"
+    },
+    {
+        "title": "Fluid Mechanics",
+        "author": "Frank M. White",
+        "stock": 4,
+        "category": "Mechanical Engineering",
+        "bookid": "book100016"
+    },
+    {
+        "title": "Engineering Mechanics: Dynamics",
+        "author": "J.L. Meriam",
+        "stock": 7,
+        "category": "Mechanical Engineering",
+        "bookid": "book100017"
+    },
+    {
+        "title": "Thermodynamics: An Engineering Approach",
+        "author": "Yunus A. Çengel",
+        "stock": 8,
+        "category": "Mechanical Engineering",
+        "bookid": "book100018"
+    },
+    {
+        "title": "Introduction to Algorithms",
+        "author": "Thomas H. Cormen",
+        "stock": 5,
+        "category": "Computer Science",
+        "bookid": "book100019"
+    },
+    {
+        "title": "Artificial Intelligence: A Modern Approach",
+        "author": "Stuart Russell",
+        "stock": 4,
+        "category": "Computer Science",
+        "bookid": "book100020"
+    },
+    {
+        "title": "Computer Networks",
+        "author": "Andrew S. Tanenbaum",
+        "stock": 6,
+        "category": "Computer Science",
+        "bookid": "book100021"
+    },
+    {
+        "title": "Operating System Concepts",
+        "author": "Abraham Silberschatz",
+        "stock": 0,
+        "category": "Computer Science",
+        "bookid": "book100022"
+    }
+]
 
 const booksTable = document.getElementsByClassName('books-section');
 console.log(books);
@@ -29,7 +184,7 @@ allBooks(books);
 function getBookName() {
     let book = document.getElementsByClassName("search")[0].value;
     let availableBooks = searchName(books, book);
-
+    
     if (availableBooks[0] === undefined) {
         booksTable[0].innerHTML = " ";
         booksTable[0].innerHTML = `<div class="noBooks">No Books Found</div>`;
@@ -38,7 +193,7 @@ function getBookName() {
         booksTable[0].innerHTML = " ";
         allBooks(availableBooks);
     }
-
+    
 }
 
 function searchName(books, bookTitle) {
